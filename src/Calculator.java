37997a21e110e2eecb7f4b2db9b87da4cced1684
Aj Pakulyte <pakulyta@tcd.ie>
2022-10-06 00:20:58 +0100

    Changing an inputted expression into numberical values and operators. Validation attempt.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -26,17 +27,62 @@ public class Calculator {
             System.out.println(operations);
             System.out.println("Please enter your mathematical expression:");
 
-            String expIn = input.next().trim();
+            String expIn = input.nextLine().trim();
 
             if (expIn.equalsIgnoreCase("Quit") || expIn.equalsIgnoreCase("End")){
                 System.out.println("Thank you for using this calculator!" + "\nCalculator quitting.");
                 quit = true;
             }
             else {
+                ArrayList<String> mathExp = nums_ops(expIn);
                 // validate string
                 // perform calculation
                 // output result
             }
         }
     }
+
+    /**
+     * Changes the given expression into numbers and operators
+     * @param expIn : expression to be converted
+     */
+    private static ArrayList<String> nums_ops(String expIn) {
+        char[] chars = expIn.toCharArray();
+        ArrayList<String> expression = new ArrayList<>();
+        String number = "";
+
+        for (int i = 0; i < chars.length; i++){
+            int j = -1;
+            if (chars[i] >= '0' && chars[i] <= '9'){
+                number += chars[i];
+            }
+            else if (chars[i] == '+' || chars[i] == '-' || chars[i] == '*'){
+                expression.add(number);
+                expression.add(String.valueOf(chars[i]));
+                number = "";
+            }
+            if (i == chars.length-1 && !number.equals("")){
+                expression.add(number);
+            }
+        }
+        return expression;
+    }
+    /**
+     * Checks if expression is valid, only numbers and no duplicate operators
+     * @param expIn : expression to be validated
+     * @return 1 if expression is valid, 0 if expression is not valid
+     */
+    private static boolean validateExp(String expIn){
+        String[] chars = expIn.split("");
+
+        for (String character : chars){
+            if (Integer.parseInt(character) < 0 || Integer.parseInt(character) > 9
+                || !character.equals("+") || !character.equals("-") || !character.equals("*")){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
 }
