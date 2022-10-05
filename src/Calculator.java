bf7023c2a144bf888632f140e7c54640f85ede01
Aj Pakulyte <pakulyta@tcd.ie>
2022-10-05 22:04:56 +0100

    Initial setup & outline.

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+/**
+ * SWENG Assignment 1 - Group 38
+ * Simple calculator app which takes in an inputted mathematical expression and returns the result of the expression
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        // get user input
+        // validate string
+        // perform calculation
+        // output result
+    }
+}
