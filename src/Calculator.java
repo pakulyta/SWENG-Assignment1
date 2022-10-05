31beb209498d10de2789ab4d30d4993dfb4a8bd6
Aj Pakulyte <pakulyta@tcd.ie>
2022-10-05 22:22:15 +0100

    Beginning of user input. Take in a string expression or quit message.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,42 @@
+import java.util.Scanner;
+
 /**
  * SWENG Assignment 1 - Group 38
  * Simple calculator app which takes in an inputted mathematical expression and returns the result of the expression
  */
 public class Calculator {
+    static String operations = ("""
+                The available operations are as follows:
+                 * Addition (+)
+                 * Subtraction (-)
+                 * Multiplication (*)
+                 * Quit (Quit/End)""");
+
     public static void main(String[] args) {
-        // get user input
-        // validate string
-        // perform calculation
-        // output result
+        boolean quit = false;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("""
+                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                  Welcome to the Calculator app!
+                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""");
+
+        //program runs until user wishes to quit
+        while (!quit){
+            System.out.println(operations);
+            System.out.println("Please enter your mathematical expression:");
+
+            String expIn = input.next().trim();
+
+            if (expIn.equalsIgnoreCase("Quit") || expIn.equalsIgnoreCase("End")){
+                System.out.println("Thank you for using this calculator!" + "\nCalculator quitting.");
+                quit = true;
+            }
+            else {
+                // validate string
+                // perform calculation
+                // output result
+            }
+        }
     }
 }
